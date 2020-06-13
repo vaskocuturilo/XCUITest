2b8dd9c47cb2db1bcb1cada063d19ecdc4de3842
asm@dostavista.ru <wolfstonik>
2020-06-13 15:46:19 +0300

    Initial Commit

---
+++ ToDoApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApplication
+//
+//  Created by Anton Smirnov on 13.06.2020.
+//  Copyright © 2020 Anton Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
