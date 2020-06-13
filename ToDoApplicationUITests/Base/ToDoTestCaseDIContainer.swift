18c9badcce480469c3dfeec229f31bf042b71428
asm@dostavista.ru <wolfstonik>
2020-06-13 17:25:24 +0300

    QA-0001 Create base class

---
+++ ToDoApplicationUITests/Base/ToDoTestCaseDIContainer.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTestCaseDIContainer.swift
+//  ToDoApplicationUITests
+//
+//  Created by Anton Smirnov on 13.06.2020.
+//  Copyright © 2020 Anton Smirnov. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+
+/// DescriptionToDoTestCaseDIContainer. This is container.
+final class ToDoTestCaseDIContainer {
+    private(set) lazy var application = XCUIApplication()
+    
+    
+    // MARK: - Init
+    init() {
+        _ = Bundle(for: ToDoTestCase.self)
+    }
+}
+
