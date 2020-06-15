a773bde20eab0990accea3a6e4bc7138c1417813
asm@dostavista.ru <wolfstonik>
2020-06-15 20:03:53 +0300

    QA-0003 Create simple test

---
+++ ToDoApplicationUITests/CheckPreconditions.swift
@@ -0,0 +1,16 @@
+//
+//  CheckPreconditions.swift
+//  ToDoApplicationUITests
+//
+//  Created by Anton Smirnov on 15.06.2020.
+//  Copyright © 2020 Anton Smirnov. All rights reserved.
+//
+
+import XCTest
+
+class CheckPreconditions: ToDoTestCase {
+
+    func testCheckPreconditions() {
+    }
+
+}
